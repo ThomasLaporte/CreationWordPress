7b3faf035ce80b7b31d15ec97fdbeea3faace5a1
Chaaampy <tony.evariste@gmail.com>
2017-01-17 17:29:49 +0100

    Add files via upload

--- traitement.php
+++ traitement.php
@@ -10,17 +10,8 @@ $SITE_id          =$_POST['SITE_id'];
 $SITE_mdp         =$_POST['SITE_mdp'];
 $SITE_mdp_confirm =$_POST['SITE_mdp_confirm'];
 $SITE_mail        =$_POST['SITE_mail'];
+$THEME_name       =$_POST['THEME_list'];
 //$SITE_index       =$_POST['SITE_index'];
-
-$textToAdd = "<Directory /var/www/html/wpcli>
-              Options Indexes FollowSymlinks
-              AllowOverride All
-              Require all granted
-              </Directory>";
-
-$textConf = "<ifModule mod_rewrite.c>
-              RewriteEngine On
-              </ifModule>";
 
 $output= array();
 $cmd='sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -40,31 +31,28 @@ wp db create
 
 wp core install --url='.$BDD_host.' --title='.$SITE_title.' --admin_user='.$SITE_id.' --admin_password='.$SITE_mdp.' --admin_email='.$SITE_mail.' --skip-email
 
+wp plugin delete hello akismet  
+
+wp plugin install duplicator wordfence duplicate-post wordpress-seo contact-form-7 --activate
+
+wp theme install '.$THEME_name.' --activate
+
+wp theme delete twentyfifteen twentysixteen twentyseventeen
 
 
-sudo sed -i \'13i\<Directory /var/www/html/wpcli>
-              Options Indexes FollowSymlinks
-              AllowOverride All
-              Require all granted
-              </Directory>\' \'/etc/apache2/sites-available/000-default.conf\'
+sudo sed -i -e \'12,17d\' /etc/apache2/sites-available/000-default.conf
 
-\'<ifModule mod_rewrite.c>
-RewriteEngine On
-</ifModule>\' >> \'/etc/apache2/apache2.conf\'
+sudo sed -i "12i\DocumentRoot /var/www/html/'.$SITE_title.'\n<Directory /var/www/html/'.$SITE_title.'>\nOptions Indexes FollowSymlinks\nAllowOverride All\nRequire all granted\n</Directory>\n" /etc/apache2/sites-available/000-default.conf
 
-sudo service apache2 restart';
+sudo service apache2 reload';
 
-$return_var=10;
 //echo $cmd;
 //echo '1 :'.escapeshellcmd($cmd);
 //echo '  <br>  2 :'.escapeshellarg($cmd);
 
-$test = exec($cmd);
+exec($cmd);
 //exec($cmd,$output,$return_var);
 
-var_dump($test);
-
- if ($return_var!=0) {
-   echo(' ERREUR !! ');
- }
 ?>
+
+<!--sudo sed -i -e "\$a<ifModule mod_rewrite.c>\nRewriteEngine On\n</ifModule>" /etc/apache2/apache2.conf-->
